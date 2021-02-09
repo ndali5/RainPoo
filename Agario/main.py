1ae3ee3f9358a9c5ff72642b663ce0acd57eb4fc
ndali5 <jeancgn@msn.com>
2021-02-09 10:04:39 +0100

    agario1

---
+++ Agario/main.py
@@ -0,0 +1,4 @@
+from Agario.player import Player
+
+if __name__ == '__main__' :
+    mon_joueur = Player()
---
+++ Agario/player.py
@@ -0,0 +1,19 @@
+class Player(object):
+
+    def __init__ (self):
+        self.taille = 2
+        self.vitesse = 2
+        self.forme = 2
+        self.direction = vector2
+        self.position = vector2
+        self.couleur = vector3
+    pass
+
+
+    def manger (self)
+
+    def mourir (self)
+
+    def deplacer (self)
+
+    def afficher (self)
